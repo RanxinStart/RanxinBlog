12057ee2ae81dff1752d91891dc89d33646ff8bf
RanxinStart <597146348@qq.com>
2021-06-28 03:09:42 +0800

    ~初始

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,52 @@
+/*
+ * @Author: Zhilong
+ * @Date: 2021-06-22 00:47:30
+ * @LastEditTime: 2021-06-28 03:07:32
+ * @Description: 
+ * @LastEditors: Zhilong
+ * @autograph: ⚠ warning!  ⚠ warning!  ⚠ warning!   ⚠野生的页面出现了!!
+ */
+module.exports = {
+  // 使用主题
+  theme: 'reco',
+  // 文档标题
+  title: "Ranxin's blog",
+  // 文档说明
+  description: 'blog for vue',
+  // 静态资源基本路径
+  base: '/zh/',
+  // 设置网站图标
+  head: [
+    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
+  ],
+  themeConfig: {
+    type: 'blog',
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      },
+      socialLinks: [     // 信息栏展示社交信息
+        { icon: 'reco-github', link: 'https://github.com/RanxinStart' },
+        { icon: 'reco-npm', link: 'https://www.npmjs.com/~ranxin' }
+      ]
+    },
+    // 导航栏配置
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '开发', icon: 'reco-document',
+        items: [
+          { text: '前端', link: '/web/01-vue3' },
+          { text: '后端', link: 'url' },
+          { text: '工具', link: 'path' }
+        ]
+      },
+    ],
+  },
+}
---
+++ docs/web/02-react.md
@@ -0,0 +1,25 @@
+---
+title: 02-react
+date: 现在的时间
+categories:
+  - react
+tags: 
+  - 标签
+
+---
+
+# Build And Config
+
+> 如何搭建安装以及全部配置项
+
+# Code And Application
+
+> 代码 和 应用场景
+
+# Important And Extend
+
+> 扩展内容 以及重点
+
+## Portals插槽
+
+> ReactDOM.createPortal(child, container)
