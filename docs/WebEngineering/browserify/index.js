bf07237f281ba6ef91bbda92d9c4d2c4c06e338a
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-08-24 14:10:08 +0800

    添加uniq 示例

--- docs/WebEngineering/browserify/index.js
+++ docs/WebEngineering/browserify/index.js
@@ -1,3 +1,14 @@
+/*
+ * @Author: Zhilong
+ * @Date: 2021-08-18 09:15:23
+ * @LastEditTime: 2021-08-24 14:09:13
+ * @Description: 
+ * @LastEditors: Zhilong
+ * @autograph: ⚠ warning!  ⚠ warning!  ⚠ warning!   ⚠野生的页面出现了!!
+ */
 
-const srcModule = require('./src/module')
-srcModule.foo()
+const srcModule = require('./src/module')  // 使用其他模块
+const uniq = require('uniq')  // 使用外部模块
+
+srcModule.foo()  // 调用其他模块的方法
+console.log('uniq',uniq([1,1,2,2,3,4,1]))  //使用外部模块的方法
