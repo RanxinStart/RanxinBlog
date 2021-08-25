14e89db53f29459c9865d7addd81ab05fbf2fd8b
燃芯 <56920162+RanxinStart@users.noreply.github.com>
2021-08-25 18:37:01 +0800

    模块化

---
+++ docs/WebEngineering/requirejs/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Zhilong
+ * @Date: 2021-08-25 09:31:36
+ * @LastEditTime: 2021-08-25 09:46:12
+ * @Description: 入口
+ * @LastEditors: Zhilong
+ * @autograph: ⚠ warning!  ⚠ warning!  ⚠ warning!   ⚠野生的页面出现了!!
+ */
+
+// 调用模块2 的方法使用
+module2.output()
---
+++ docs/WebEngineering/requirejs/noRequireJs/module1.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Zhilong
+ * @Date: 2021-08-25 09:36:57
+ * @LastEditTime: 2021-08-25 09:49:13
+ * @Description: 
+ * @LastEditors: Zhilong
+ * @autograph: ⚠ warning!  ⚠ warning!  ⚠ warning!   ⚠野生的页面出现了!!
+ */
+/* 没有依赖的模块 */
+(()=>{
+    const getName = () => "module1"
+    return window.module1 = { getName }
+})()
